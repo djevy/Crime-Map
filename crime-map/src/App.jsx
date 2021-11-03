1446d7efa5eb9ce5a4eacbb73593dd50fa65a65a
Darren <60827567+djevy@users.noreply.github.com>
2021-11-03 20:28:40 +0000

    chore: Add leaflet mao

--- crime-map/src/App.jsx
+++ crime-map/src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   return (
     <div className="App">
       <h1>Crime mapper</h1>
+      {/* <div id="map"></div> */}
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="mapstyle">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
